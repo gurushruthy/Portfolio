5d8afc98b5b3b65dc0207247e0bc8f9d6440172d
guru shruthy <gurushruthy@posterchild.ai>
2025-11-30 15:25:41 -0800

    Initial commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Guru Shruthy",
+  profile: '/profile.jpg',
+  designation: "Turn Ideas into Code",
+  description: "Hey there, I'm Shruthy!  I'm that developer who gets unreasonably excited about clean code and stays up way too late debugging. When not chasing the perfect solution, I'm probably exploring some shiny new tech that caught my attention. My learning style? Dive in headfirst and figure out how to swim while falling! Apparently I'm also learning to skate which is a bit of a surprise.",
+  description2: "My code is so meticulously organized they make my sock drawer look chaotic—I've been known to refactor perfectly working functions at 2 AM just because the indentation felt wrong. My superpower? Making complex systems look so simple your grandmother could use them. I believe good documentation is like flossing - everyone knows they should do it, but I'm one of the few who actually does(both!). Currently seeking opportunities where I can build meaningful tech, preferably at places that don't mind the occasional dad joke in code comments. Let's create something awesome together!",
+  email: 'gurushruthy.g@gmail.com',
+  phone: '+12602068290',
+  address: 'Sunnyvale CA, USA',
+  github: 'https://github.com/gurushruthy',
+  facebook: 'https://www.facebook.com/shruthy.moorthy.2025/',
+  linkedIn: 'https://www.linkedin.com/in/guru-shruthy-guru-moorthy-22362b1b4/',
+  twitter: 'https://x.com/gurushruthy',
+  stackOverflow: 'https://stackoverflow.com/users/29148012/neha-nooka',
+  leetcode: "https://leetcode.com/u/nehanooka/",
+  devUsername: "shruthy",
+  resume: "https://drive.google.com/file/d/1baPWUK4sQbP-NiWEjL_gHpA6nOTgGv83/view?usp=drive_link"
+}
