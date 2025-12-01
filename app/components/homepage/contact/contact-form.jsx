b88b842876157755bc1739ec985a4f01216d4b40
guru shruthy <gurushruthy@posterchild.ai>
2025-11-30 22:50:10 -0800

    update contact

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,10 +36,10 @@ function ContactForm() {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+        `/api/contact`,
         userInput
       );
-
+      
       toast.success("Message sent successfully!");
       setUserInput({
         name: "",
